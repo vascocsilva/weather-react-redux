d8a5afcc19b35fad767972d4ce5e818dcaee3b45
Vasco Silva <vasco.s.rebolo@gmail.com>
2016-10-07 15:36:43 +0100

    Add api request for weather on submit of search bar (action), add action

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,21 +1,34 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
     this.setState({term: event.target.value});
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form
+        className="input-group"
+        onSubmit={this.onFormSubmit}>
         <input
           placeholder="Get a five day forecast in your favourite cities"
           className="form-control"
@@ -28,3 +41,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
